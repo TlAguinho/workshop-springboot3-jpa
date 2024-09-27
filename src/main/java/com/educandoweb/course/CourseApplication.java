d3165c7f9d5a5f5f9ebb6ba52308e88fa8b04f6b
Xbrain <xbrain@MacBook-Air-de-Miriany.local>
2024-09-26 23:36:26 -0300

    Project created

---
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -0,0 +1,12 @@
+package com.educandoweb.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //anotacion para configurar esse projeto para que ele seja uma aplicação spring boot.
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+}
